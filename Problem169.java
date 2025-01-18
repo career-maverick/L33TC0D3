c5bed50c017fb3dc87519142df5768a20cc4ca8f
Chiranjeevi Ramamurthy <chiranram7@live.com>
2025-01-18 16:05:10 -0700

    Added java soltion for p169 majority element

---
+++ Problem169.java
@@ -0,0 +1,16 @@
+public class Problem169 {
+    public static void main(String[] args) {
+        int[] nums = {3, 2, 3};
+        System.out.println(majorityElement(nums)); 
+    }
+    public static int majorityElement(int[] nums) {
+        int count = 0, candidate = 0;
+        for (int num : nums) {
+            if (count == 0) {
+                candidate = num;
+            }
+            count += (num == candidate) ? 1 : -1;
+        }
+        return candidate;
+    }
+}
